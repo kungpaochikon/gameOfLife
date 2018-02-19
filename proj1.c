455c2f83ba864f3c12eb8ec90ced3660f41f9190
Gary Fleming <flemingg@mail.gvsu.edu>
2018-02-19 02:01:26 -0500

    made board local and did some cleaning, still more to do I think

--- proj1.c
+++ proj1.c
@@ -3,11 +3,6 @@
 #include <string.h>
 #include <math.h>
 #include "file_utilities.h"
-
-int **board = NULL;//Couldn't get a triple pointer working :(
-int sizeX;//Bunch of globals, sorry
-int sizeY;
-char *fileName = "test.txt";//Hardcoded Filename O____O
 /**
  *  Main Function
  *  -------------
@@ -15,6 +10,10 @@ char *fileName = "test.txt";//Hardcoded Filename O____O
  *  for input. 
  */
 int main(int argc, char *argv[]){
+  int **board;
+  int i,j;
+  int sizeX,sizeY;
+  char *fileName = "test.txt";
   if(argc>2){
     sizeX = atoi(argv[1]);
     sizeY = atoi(argv[2]);
@@ -23,7 +22,7 @@ int main(int argc, char *argv[]){
     printf("Not enough arguments! Shutting down...\n");
     exit(1);
   }
-  makeBoard();
+  makeBoard(&board,sizeX,sizeY);
   if(sizeX>7 && sizeY>7){
     board[5][5] = 1;
     board[5][6] = 1;
@@ -34,31 +33,38 @@ int main(int argc, char *argv[]){
     board[1][1] = 1;
     board[2][0] = 1;
   }
-  printBoard();
+  printBoard(board,sizeX,sizeY);
   //Input code from https://stackoverflow.com/questions/22065675/get-text-from-user-input-using-c
   char input[20];
   input[0] = '0';
   while(1){
     printf("What now?\n");
     printf("1.) Step\n");
-    printf("2.) Save\n");
-    printf("3.) Load\n");
-    printf("4.) Quit\n");
+    printf("2.) Step #_OF_TIMES\n");
+    printf("3.) Save\n");
+    printf("4.) Load\n");
+    printf("5.) Quit\n");
     fgets(input,20,stdin);
     switch(input[0]){
       case '1':
-	stepBoard();
-	printBoard();
+	stepBoard(&board,sizeX,sizeY);
+	printBoard(board,sizeX,sizeY);
 	break;
       case '2':
-	saveBoard();
+	for(i = 0;i<input[3];i++){
+	  stepBoard(&board,sizeX,sizeY);
+	  printBoard(board,sizeX,sizeY);
+	}
 	break;
       case '3':
-	loadBoard();
+	saveBoard(board,sizeX,sizeY,fileName);
 	break;
       case '4':
+	loadBoard(&board,&sizeX,&sizeY,fileName);
+	break;
+      case '5':
 	printf("Shutting Down...\n");
-	clearBoard();
+	clearBoard(&board);
 	exit(1);
 	break;
     }
@@ -73,18 +79,16 @@ int main(int argc, char *argv[]){
  *  with a global array for now, couldn't figure out 
  *  triple pointers. Uses malloc for memory management.
  */
-int makeBoard(){
-  int row = sizeX;
-  int col = sizeY;
+int makeBoard(int ***board,int sizeX,int sizeY){
   int i,j;
-  board = (int **)malloc(row * sizeof(int*));
-  for (i = 0;i < row; i++){
-    board[i] = (int *)malloc(col * sizeof(int));
+  *board = (int**)malloc(sizeX * sizeof(int*));
+  for (i = 0;i < sizeX; i++){
+    (*board)[i] = (int*)malloc(sizeY * sizeof(int));
   }
   
-  for (i = 0;i<row;i++){
-    for(j = 0;j<col;j++){
-      board[i][j] = 0;
+  for (i = 0;i<sizeX;i++){
+    for(j = 0;j<sizeY;j++){
+      (*board)[i][j] = 0;
     }
   }
   
@@ -97,8 +101,11 @@ int makeBoard(){
  *  Prints the board by just looping through
  *  the 2d array using a nested for loop.
  *  Chooses symobls based on value.
+ *
  */
-int printBoard(){
+
+
+int printBoard(int **board,int sizeX,int sizeY){
   int i,j;
   char sym;
   printf("\n");
@@ -119,6 +126,7 @@ int printBoard(){
   return 0;
 }
 
+
 /*
  *  stepBoard()
  *  -----------
@@ -128,7 +136,9 @@ int printBoard(){
  *  then marking for death/life based on
  *  the findings, then finally updating.
  */
-int stepBoard(){
+
+
+int stepBoard(int ***board,int sizeX, int sizeY){
   int marked[sizeX*sizeY];//marked for change
   int i,j;
   for(i = 0;i<sizeX*sizeY;i++){
@@ -143,18 +153,18 @@ int stepBoard(){
 	for(y = j-1;y<j+2;y++){
 	  //Check Bounds
 	  if(x>=0 && x<sizeX && y>=0 && y<sizeY && (x!=i || y!=j)){
-	    if(board[x][y] == 1){
+	    if((*board)[x][y] == 1){
 	      neighbors = neighbors + 1;
 	    }
 	  }
 	}
       }
       //If Living
-      if(board[i][j] == 1 && (neighbors > 3 || neighbors < 2)){
+      if((*board)[i][j] == 1 && (neighbors > 3 || neighbors < 2)){
 	marked[sizeX*(j) + i] = 1;
       }
       //If dead
-      if(board[i][j] == 0 && neighbors == 3){
+      if((*board)[i][j] == 0 && neighbors == 3){
 	marked[sizeX*(j) + i] = 1;
       }
       
@@ -164,17 +174,18 @@ int stepBoard(){
   for(i = 0;i<sizeX;i++){
     for(j = 0;j<sizeY;j++){
       if(marked[sizeX * (j) + i] == 1){
-	if(board[i][j]==1){
-	  board[i][j] = 0;
+	if((*board)[i][j]==1){
+	  (*board)[i][j] = 0;
 	}
 	else{
-	  board[i][j] = 1; 
+	  (*board)[i][j] = 1; 
 	};//Flip
       }
     }
   }
   return 0;
 }
+
 
 /*
  *  clearBoard
@@ -184,12 +195,14 @@ int stepBoard(){
  *  because I don't use local vars, so I'm not sure
  *  if I can init another global array after freeing.
  */
-int clearBoard(){
+
+int clearBoard(int ***board,int sizeX){
   int i;
+  printf("sizeX: %i\n",sizeX);
   for(i = 0;i<sizeX;i++){
-    free(board[i]);
+    free((*board)[i]);
   }
-  free(board);
+  free(*board);
   return 0;
 }
 
@@ -198,7 +211,10 @@ int clearBoard(){
  *  --------
  *  Reallocs the memory for the global board.
  *  Currently unused.
+ *
  */
+
+/*
 int newBoard(){
   int i,j;
   board = (int **)realloc(board,sizeX * sizeof(int*));
@@ -213,19 +229,23 @@ int newBoard(){
   }
   return 0;
 }
+*/
 
 /*
+ * 
  *  popBoard
  *  --------
  *  Populates the board based on a parsed string
  *  of characters (for loading, etc.)
  */
-int popBoard(char *mark){
+
+
+int popBoard(int ***board,int sizeX,int sizeY, char *mark){
   int i,j;
   for(i = 0;i<sizeX;i++){
     for(j = 0;j<sizeY;j++){
       if(mark[sizeX * (j) + i] == '1'){
-	board[i][j] = 1;
+	(*board)[i][j] = 1;
       }
     }
   }
@@ -238,7 +258,9 @@ int popBoard(char *mark){
  *  in order to create a new board and populate it
  *  based on the data given.
  */
-int loadBoard(){
+
+
+int loadBoard(int ***board,int *sizeX, int *sizeY, char *fileName){
   char *buffer;
   int size = read_file(fileName,&buffer);
   char *sX,*sY;
@@ -258,12 +280,13 @@ int loadBoard(){
     }
     sizeSX = sizeSX + 1;
   }
-  sX = (char *)malloc(sizeSX * sizeof(char));
+  sX = (char *)malloc((sizeSX+1) * sizeof(char));
   for(i = 0;i<sizeSX;i++){
     sX[i] = buffer[i];
   }
-  sizeX = atoi(sX);
-  printf("SizeX: %i\n",sizeX);
+  sX[i] = '\0';
+  *sizeX = atoi(sX);
+  printf("SizeX: %i\n",*sizeX);
   //y
   sizeSY = 0;
   for(i = sizeSX+1;i<size;i++){
@@ -272,25 +295,30 @@ int loadBoard(){
     }
     sizeSY = sizeSY + 1;
   }
-  sY = (char *)malloc(sizeSY * sizeof(char));
+  sY = (char *)malloc((sizeSY+1) * sizeof(char));
   for(i = 0;i<sizeSY;i++){
     sY[i] = buffer[sizeSX+1+i];
   }
-  sizeY = atoi(sY);
-  printf("sizeY: %i\n",sizeY);
+  sY[i] = '\0';
+  *sizeY = atoi(sY);
+  printf("sizeY: %i\n",*sizeY);
   
   char *mark;
-  mark = (char *)malloc(sizeX * sizeY * sizeof(char));
-  for(i = 0;i<sizeX * sizeY;i++){
+  mark = (char *)malloc(*sizeX * *sizeY * sizeof(char));
+  for(i = 0;i<*sizeX * *sizeY;i++){
     mark[i] = buffer[sizeSX+sizeSY+2+i];
   }
   printf("\n");
-  for(i = 0;i<sizeX*sizeY;i++){
+  for(i = 0;i<*sizeX * *sizeY;i++){
     printf("%c",mark[i]);
   }
-  makeBoard();
-  popBoard(mark);
-  printBoard();
+  printf("\n---)%i\n",*sizeX);
+  printf("---)%i\n",*sizeY);
+  printf("--)%s\n",sX);
+  //clearBoard(board,*sizeX);
+  makeBoard(board,*sizeX,*sizeY);
+  popBoard(board,*sizeX,*sizeY,mark);
+  printBoard(*board,*sizeX,*sizeY);
   free(sY);
   free(sX);
   free(buffer);
@@ -303,7 +331,9 @@ int loadBoard(){
  *  uses file_write to write the data in the specified
  *  format to be parsed later by loadBoard.
  */
-int saveBoard(){
+
+
+int saveBoard(int **board, int sizeX, int sizeY, char *fileName){
   char *buffer;
   int i,j;
   int offset = 2;
@@ -316,6 +346,7 @@ int saveBoard(){
   for(i = 0;i<size;i++){
     buffer[i] = ' ';
   }
+  
   char numX[5];
   char numY[5];
   sprintf(numX,"%d",sizeX);
@@ -337,6 +368,8 @@ int saveBoard(){
     printf("%c",buffer[i]);
   }
   write_file(fileName,buffer,size);
-  printBoard();
+  //printBoard(board,sizeX,sizeY);
   return 0;
 }
+
+
